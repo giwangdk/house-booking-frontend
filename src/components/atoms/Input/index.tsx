a37b1e6e39a7ded161f8e22f576c4dbc7e37422a
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-14 15:55:14 +0700

    feat: layouting input component

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames';
+import React from 'react';
+import { InputProps } from '../interface';
+import style from './index.module.scss';
+
+const Input: React.FC<InputProps> = (props) => {
+  const {
+    type = 'text',
+    name,
+    placeholder,
+    errors,
+    value,
+    onChange,
+    disabled = false,
+  } = props;
+
+  const classProps = classNames(
+    style.input,
+    disabled ? style.input__disabled : '',
+    errors ? style.input__error : '',
+  );
+  return (
+    <input
+      type={type}
+      className={classProps}
+      name={name}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      disabled={disabled}
+    />
+  );
+};
+
+export default Input;
