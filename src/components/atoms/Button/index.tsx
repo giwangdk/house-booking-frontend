d71279bd06c0fbe488334f59c3609197a5fec799
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-14 15:51:18 +0700

    feat: layouting Button

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import style from './index.module.scss';
+import { ButtonProps } from '../interface';
+import classNames from 'classnames';
+import { ClipLoader } from 'react-spinners';
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const { children, onClick, variant = 'primary', type, loading } = props;
+  const className = classNames(
+    style.button,
+    style[variant],
+    loading && style.loading,
+  );
+  return (
+    <button
+      onClick={onClick}
+      data-testid="button-test-id"
+      className={className}
+      type={type}
+      disabled={loading}
+    >
+      {loading ? (
+        <div className={style.button__loading}>
+          <ClipLoader size={20} />
+        </div>
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
+
+export default Button;
