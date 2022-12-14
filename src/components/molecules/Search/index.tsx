0162a28e9a417660f8eb3147f6e3dbd19b6707eb
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-14 16:27:40 +0700

    feat: layouting search component

---
+++ src/components/molecules/Search/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './index.module.scss';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { SearchProps } from '../interface';
+
+const Search: React.FC<SearchProps> = ({
+  handleSearch,
+  value,
+}): JSX.Element => {
+  return (
+    <div className={style.search}>
+      <AiOutlineSearch />
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={handleSearch}
+        value={value}
+      />
+    </div>
+  );
+};
+
+export default Search;
