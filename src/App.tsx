4c3c7d1223cf999e057da80c012e1c53e9f320ee
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-15 14:39:15 +0700

    feat: add toast and suspense tag to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { ToastContainer } from 'react-toastify';
+import Skeleton from 'react-loading-skeleton';
+
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense
+      fallback={
+        <div style={{ margin: '10rem' }}>
+          <Skeleton
+            style={{
+              margin: '1rem auto',
+              width: '100%',
+              textAlign: 'center',
+            }}
+            count={5}
+          />
+        </div>
+      }
+    >
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Suspense>
   );
 }
 
