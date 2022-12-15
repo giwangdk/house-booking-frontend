f92ba6a5748279677d8398bf87283599d30e02fc
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-15 09:26:01 +0700

    feat: create service auth

---
+++ src/services/auth.service.ts
@@ -0,0 +1,8 @@
+
+import { IUser } from '../helpers/types';
+import { postWithoutHeader } from './config';
+
+const API_ACCOUNT = process.env.REACT_APP_ACCOUNT_SERVICES_URL;
+
+// Auth
+export const submitLogin = postWithoutHeader<IUser>(`${API_ACCOUNT}/login`);
