37dd9d9947789c171a2ebf56ca326ef7fdb0aa57
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-15 09:26:52 +0700

    feat: add error compontn to input label component

--- src/components/molecules/InputLabel/index.tsx
+++ src/components/molecules/InputLabel/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import style from './index.module.scss';
-import { Input } from '../../atoms';
+import { Error, Input } from '../../atoms';
 import { InputLabelProps } from './interface ';
 
 const InputLabel: React.FC<InputLabelProps> = (props) => {
-  const { label = 'label', ...rest } = props;
+  const { label = 'label', message, ...rest } = props;
   return (
     <div className={style.form__input}>
       <label className={style.form__input__label}>{label}</label>
       <Input {...rest} />
+      <Error message={message} />
     </div>
   );
 };
