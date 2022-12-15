7b4fda5cf43e48b04f05711e8ceba361a8431bc9
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-15 09:25:48 +0700

    feat: add config api endpoint and method

---
+++ src/services/config.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+const AxiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_ACCOUNT_SERVICES_URL,
+});
+
+const cookies = new Cookies();
+const token = cookies.get('token');
+AxiosInstance.interceptors.request.use(
+  (config?: any) => {
+    if (!cookies.get('token')) {
+      config.headers.Authorization = `Bearer ${cookies.get('token')}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      if (token) {
+        cookies.remove('token');
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+const postWithoutHeader =
+  <T>(api: string) =>
+  (data: T) => {
+    return axios.post(api, data, {
+      method: 'POST',
+    });
+  };
+
+const get = (api: string) => (params?: string) => {
+  return AxiosInstance(api, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${cookies.get('token')}`,
+    },
+    params,
+  });
+};
+
+const getWithSlug = (api: string) => (slug?: string, params?: string) => {
+  return AxiosInstance(`${api}?${slug}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${cookies.get('token')}`,
+    },
+    params,
+  });
+};
+
+const post =
+  <T>(api: string) =>
+  (data: T, params?: string) => {
+    return AxiosInstance.post(api, data, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${cookies.get('token')}`,
+      },
+      params,
+    });
+  };
+export { postWithoutHeader, get, getWithSlug, post };
