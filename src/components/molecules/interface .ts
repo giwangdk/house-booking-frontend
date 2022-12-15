f8c37db6117a46765b771b918d7b3479ffea3a5e
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-15 14:39:46 +0700

    feat: add interface search and dropdown

---
+++ src/components/molecules/interface .ts
@@ -0,0 +1,22 @@
+import { InputProps } from '../atoms/interface';
+
+export interface InputLabelProps extends InputProps {
+  label: string;
+  message: string | undefined ;
+}
+
+
+export interface DropdownProps {
+  name?: string;
+  value?: string;
+  values?: {
+    value: string | number;
+    label: string;
+  }[];
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export interface SearchProps {
+  handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+}
