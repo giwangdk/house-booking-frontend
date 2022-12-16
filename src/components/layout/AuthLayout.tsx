07dd464d306df61a5ea05f62aa0a223652af9e48
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-16 11:31:15 +0700

    feat: add props layout, styling, delete auth hero component

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { AuthHero } from '../organisms';
 import style from './index.module.scss';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
   image?: string;
+  layout:string;
 }
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({
   children,
   image,
+  layout,
+
 }): JSX.Element => {
   return (
     <div className={style.auth__layout}>
       <div className={style.auth__layout__content}>
-        <div className={style.auth__layout__content__icon}>
-          <AuthHero image={image} />
+        <div className={`${style.auth__layout__content__icon} ${style[layout]}`}>
+          <img src={image} alt="" />
         </div>
         <div className={style.auth__layout__content__form}>{children}</div>
       </div>
