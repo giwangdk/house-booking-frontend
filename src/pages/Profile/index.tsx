590ec839a482ca889f63f49df7e3558eab11d253
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-16 15:03:18 +0700

    feat: styling profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { CardProfile, CardWallet, Container } from '../../components';
 import useAuth from '../../hooks/useAuth';
 import { getUser } from '../../redux/authenticationSlice';
 import style from './index.module.scss';
@@ -7,18 +8,23 @@ import style from './index.module.scss';
 const Profile = (): JSX.Element => {
   const dispatch = useDispatch();
 
-
   useEffect(() => {
     getUser()(dispatch);
   }, [dispatch]);
 
   const { user } = useAuth();
 
-console.log(user);
+  console.log(user);
 
   return (
     <div className={style.profile__page}>
-
+      <Container className={style.profile__page__content}>
+        <div className={style.profile__page__content__left}>
+          <CardProfile />
+          <CardWallet />
+        </div>
+        <div className={style.profile__page__content__right}></div>
+      </Container>
     </div>
   );
 };
