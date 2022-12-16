ed38993fd474e9909250a0ce15990653fd7893e0
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-16 15:04:07 +0700

    feat: export cards component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -3,5 +3,17 @@ import InputLabel from './InputLabel';
 import Search from './Search';
 import Table from './Table';
 import Navbar from './Navbar';
+import Card from './Card';
+import CardProfile from './CardProfile';
+import CardWallet from './CardWallet';
 
-export { Dropdown, InputLabel, Search, Table, Navbar };
+export {
+  Dropdown,
+  InputLabel,
+  Search,
+  Table,
+  Navbar,
+  Card,
+  CardProfile,
+  CardWallet,
+};
