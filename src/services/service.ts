bf6f700269360dbc3a3918e9ab820acd649279ad
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-22 10:37:41 +0700

    feat: add func to get wallet and edit user

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,7 +1,7 @@
-import { get } from './config';
+import { get, put } from './config';
 
 const API_ACCOUNT = process.env.REACT_APP_ACCOUNT_SERVICES_URL;
 
-
 export const getUserDetails = get(`${API_ACCOUNT}/user`);
-
+export const getWalletUser = get(`${API_ACCOUNT}/wallet`);
+export const EditUser = put(`${API_ACCOUNT}/user`);
