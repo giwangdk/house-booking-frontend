60aa5bb2f2c16be8c9a66d8ca098f6b3ccca332a
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-22 10:39:34 +0700

    feat: integrasi user waller to profile

--- src/helpers/types/entity.interface.ts
+++ src/helpers/types/entity.interface.ts
@@ -1,14 +1,19 @@
-export interface ICity{
-    id: number;
-    name: string;
+export interface ICity {
+  id: number | undefined;
+  name: string | undefined;
 }
 
-export interface IUser{
-    id?: number;
-    fullname?: string;
-    email?: string;
-    password: string;
-    address?: string;
-    photo?: string;
-    city?: string;
+export interface IUser {
+  id?: number;
+  fullname?: string;
+  email?: string;
+  password: string;
+  address?: string;
+  photo?: string;
+  city?: ICity;
+}
+
+export interface IWallet {
+  ID?: number;
+  balance?: number;
 }
