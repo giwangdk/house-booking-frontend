a3690f108f5143a7d5fceb457b4954013204a29f
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-22 10:41:39 +0700

    feat: styling navbar

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,5 +1,15 @@
-import CardLogin from "./CardLogin";
-import CardRegister from "./CardRegister";
-import Container from "./Container";
+import CardLogin from './CardLogin';
+import CardRegister from './CardRegister';
+import Container from './Container';
+import CardProfile from './CardProfile';
+import CardWallet from './CardWallet';
+import CardDetailProfile from './CardDetailProfile';
 
-export {  CardLogin, CardRegister, Container}
+export {
+  CardLogin,
+  CardRegister,
+  Container,
+  CardProfile,
+  CardWallet,
+  CardDetailProfile,
+};
--- src/helpers/types/auth.interface.ts
+++ src/helpers/types/auth.interface.ts
@@ -1,52 +1,54 @@
-import React from "react";
-import { IUser } from "./entity.interface";
+import React from 'react';
+import { IUser, IWallet } from './entity.interface';
 
-export interface AuthState{
-    user: IUser | null;
-    isLoggedIn: boolean;
-    isLoading: boolean;
-    error: string | null;
-    isError: boolean;
+export interface AuthState {
+  user: IUser | null;
+  wallet: IWallet | null;
+  isLoggedIn: boolean;
+  isLoading: boolean;
+  error: string | null;
+  isError: boolean;
 }
 
-export interface LoginProps{
-    email: string;
-    password: string;
+export interface LoginProps {
+  email: string;
+  password: string;
 }
-
 
 export interface ErrorLogin {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface FormReturnLogin<T> {
-    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-    values: T;
-    errors: ErrorLogin | undefined;
-  }
-
-  export interface RegisterProps{
-    name:string;
-    email: string;
-    address?: string;
-    city:string;
-    password: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  values: T;
+  errors: ErrorLogin | undefined;
 }
 
+export interface RegisterProps {
+  name: string;
+  email: string;
+  address?: string;
+  city: string;
+  password: string;
+}
 
 export interface ErrorRegister {
-    name:string;
-    email: string;
-    city:string;
-    password: string;
-
+  name: string;
+  email: string;
+  city: string;
+  password: string;
 }
 
 export interface FormReturnRegister<T> {
-    handleChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void;
-    handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-    values: T;
-    errors: ErrorRegister| undefined;
-  }
+  handleChange: (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>,
+  ) => void;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  values: T;
+  errors: ErrorRegister | undefined;
+}
