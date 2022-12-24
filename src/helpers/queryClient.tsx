33a2a757d21bb1fb60abb5f559cb9de7f507e219
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-24 15:49:02 +0700

    feat: add error if 401

--- src/helpers/queryClient.tsx
+++ src/helpers/queryClient.tsx
@@ -1,15 +1,26 @@
 import { MutationCache, QueryCache, QueryClient } from 'react-query';
 import { toast } from 'react-toastify';
+import Cookies from 'universal-cookie';
+
+const cookie = new Cookies();
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: async (error: any) => {
-      toast.error(error.response.data.message);
+      if (error.response.status === 401) {
+        cookie.remove('token');
+        window.location.href = '/login';
+        toast.error(error.response.data.message);
+      }
     },
   }),
   mutationCache: new MutationCache({
     onError: async (error: any) => {
-      toast.error(error.response.data.message);
+      if (error.response.status === 401) {
+        cookie.remove('token');
+        window.location.href = '/login';
+        toast.error(error.response.data.message);
+      }
     },
   }),
 });
