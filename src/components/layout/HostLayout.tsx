aab0e1c8b2d74329a122e2612fca9a8a57a861e7
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-27 22:31:11 +0700

    feat: create layout for host role

--- src/components/layout/HostLayout.tsx
+++ src/components/layout/HostLayout.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import jwt_decode from 'jwt-decode';
+import Cookies from 'universal-cookie';
 
 const HostLayout = (): JSX.Element => {
-  const { user } = useAuth();
   const location = useLocation();
-  const dispatch = useDispatch();
 
-  if (user?.role !== 'host') {
+  const cookie = new Cookies();
+
+  const decodedToken: {
+    user: {
+      role: string;
+    };
+  } = jwt_decode(cookie.get('token'));
+  console.log(decodedToken);
+
+  if (decodedToken?.user?.role !== 'host') {
     return <Navigate to="/" state={{ from: location }} replace />;
   }
 
