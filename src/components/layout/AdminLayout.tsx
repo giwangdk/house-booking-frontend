c244bde36db1c422d633e525a824c7937bbea2a6
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-29 18:49:07 +0700

    feat: layouting layout admin protected page

---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import jwt_decode from 'jwt-decode';
+import Cookies from 'universal-cookie';
+
+const HostLayout = (): JSX.Element => {
+  const location = useLocation();
+
+  const cookie = new Cookies();
+
+  const decodedToken: {
+    user: {
+      role: string;
+    };
+  } = jwt_decode(cookie.get('token'));
+  console.log(decodedToken);
+
+  if (decodedToken?.user?.role !== 'admin') {
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default HostLayout;
---
+++ src/components/layout/AppAdminLayout.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Navbar, Sidebar } from '../molecules';
+import { Container } from '../organisms';
+import style from './index.module.scss';
+
+interface AppAdminLayoutProps {
+  children: React.ReactNode;
+  image?: string;
+  layout: string;
+}
+
+const AppAdminLayout: React.FC = (): JSX.Element => {
+  return (
+    <div className={style.host__layout}>
+      <Sidebar />
+      <div className={style.host__layout__content}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AppAdminLayout;
